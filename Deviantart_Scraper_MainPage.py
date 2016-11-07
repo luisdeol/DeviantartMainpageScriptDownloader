d48738066975768a976db38f458814476c6fd411
luisfelipekai <luisfelipekai@gmail.com>
2016-11-07 11:17:02 -0300

    Fix PEP8 code style errors, except Tabs

--- Deviantart_Scraper_MainPage.py
+++ Deviantart_Scraper_MainPage.py
@@ -1,23 +1,23 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib
-import requests
-import re 
+import re
 import os
 from datetime import datetime
+
 
 c = 0
 url = "http://www.deviantart.com/browse/all/?offset="
 count = 0
-path = "C:/Users/luisdeolpy/Pictures/Deviantart_Script/" + datetime.now().strftime('%Y-%m-%d %H:%M:%S').replace(":","-")
-
+path = "C:/Users/luisdeolpy/Pictures/Deviantart_Script/" + \
+	datetime.now().strftime('%Y-%m-%d %H:%M:%S').replace(":", "-")
 os.makedirs(path)
 
-while(c<101):
-	html = urllib.request.urlopen(url+str(c)).read()
+while (c < 101):
+	html = urllib.request.urlopen(url + str(c)).read()
 	sopa = BeautifulSoup(html, "html.parser")
 	for img in sopa.find_all("img", class_=""):
-		if img.find(src = re.compile("/avatars")):
+		if img.find(src=re.compile("/avatars")):
 			pass
 		elif img['src'] == "http://a.deviantart.net/avatars/default.gif":
 			pass
